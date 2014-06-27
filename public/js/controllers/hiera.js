fa82e88222e6bcab0e5fad56ba4c7b15f5976208
Walter Heck <walterheck@olindata.com>
2014-06-27 11:01:23 +0200

    Hiera basics after playing with angular for a bit

--- public/js/controllers/hiera.js
+++ public/js/controllers/hiera.js
@@ -1,2 +1,7 @@
-ptApp.controller('HieraCtrl', function ($scope) {
+ptApp.controller('HieraCtrl', function ($scope, $routeParams, $http) {
+
+    $http.get('/hiera/hierarchies').success(function (data) {
+        $scope.contents = '' + data;
+    });
+
 });
