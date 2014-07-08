b7fff1190f73ad010719e056e3e3b8e904b4b3a1
Walter Heck <walterheck@olindata.com>
2014-07-08 19:18:32 +0200

    Initial hiera config loading

--- public/js/controllers/hiera.js
+++ public/js/controllers/hiera.js
@@ -1,5 +1,7 @@
 ptApp.controller('HieraCtrl', function ($scope, $routeParams, $http) {
+
     $http.get('/hiera/hierarchies').success(function (data) {
-        $scope.content = '' + data;
+        $scope.hieraArchitecture = '' + data;
     });
+
 });
