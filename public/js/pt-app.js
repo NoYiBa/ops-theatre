1a385111a3e0ea77cf24778a8d2ed3a1149c377d
Walter Heck <walterheck@olindata.com>
2014-07-08 19:18:12 +0200

    Adding bootstrap dashboard example from http://getbootstrap.com/examples/dashboard

--- public/js/pt-app.js
+++ public/js/pt-app.js
@@ -2,13 +2,17 @@ var ptApp = angular.module('ptApp', [ 'ngRoute', 'ui.bootstrap', 'ui.ace' ]);
 
 ptApp.config([ '$routeProvider', function ($routeProvider) {
     $routeProvider.
-        when('/hiera', { 
+        when('/hiera', {
             templateUrl : 'partials/hiera.html',
             controller  : 'HieraCtrl'
         }).
         when('/manifests/:filename', {
             templateUrl : 'partials/manifest.html',
             controller  : 'ManifestCtrl'
+        }).
+        when('/dashboard', {
+            templateUrl : 'partials/sample.html',
+            // controller  : 'LoginCtrl'
         }).
         when('/login', {
             templateUrl : 'partials/login.html',
